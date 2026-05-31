f244553ba815e66d02e7b3f1aa8a08a16571373c
jgodwin10 <easypeasy725@gmail.com>
2026-05-31 14:23:48 +0100

    update

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@ set -e
 
 python manage.py collectstatic --no-input
 python manage.py migrate
-gunicorn arp_api.wsgi:application --bind 0.0.0.0:${PORT:-8000} --workers 2
+gunicorn arp_api.wsgi:application --bind 0.0.0.0:${PORT:-8000} --workers 2 --timeout 120 --keep-alive 5
